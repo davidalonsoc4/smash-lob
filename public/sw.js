0f805057bd2ae4f6078eb4f534f5d20dd703db29
Davo <davidalonsoc4@gmail.com>
2026-09-15 18:05:29 +0200

    chore(release): prepare v1.14.0

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "smash-lob-v1.13.8"
+const CACHE_VERSION = "smash-lob-v1.14.0"
 const APP_SHELL = [
   "/offline",
   "/manifest.webmanifest",
--- src/lib/appVersion.ts
+++ src/lib/appVersion.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "1.13.8";
+export const APP_VERSION = "1.14.0";
 
-export const APP_VERSION_LABEL = "v1.13.8";
+export const APP_VERSION_LABEL = "v1.14.0";
